e5ec439a6b7cb8f183d40377023b9acf81789777
adrian <adrian.kuhn@arcor.de>
2024-08-18 17:50:20 +0200

    resolve includes

--- modelgen/src/script.groovy
+++ modelgen/src/script.groovy
@@ -1,20 +1,33 @@
+//file:noinspection GroovyAssignabilityCheck
 import groovy.xml.XmlParser
 import groovy.namespace.QName
+/* only work with patch-2 of my fork */
 
-String path = "./../../resources/peppol-bis-invoice-3/structure/syntax/ubl-invoice.xml"
+String path = "./../../resources/peppol-bis-invoice-3/structure/syntax"
+String rootFile = "$path/ubl-invoice.xml"
 XmlParser parser = new XmlParser(namespaceAware: false)
-Node root = parser.parse(path)
+Node root = parser.parse(rootFile)
+//resolving includes
+List<Node> includeNodes = root.'**'.findAll{ Node node ->
+    def name = node.name()
+    return (name instanceof QName &&
+            name.localPart == 'Include')
+}
+includeNodes.each { Node node ->
+    String includePath = node.text()
+    Node nodeToInclude = parser.parse("$path/$includePath")
+    node.replaceNode(nodeToInclude)
+}
+
 List<Node> references = root.'**'.findAll {Node node ->
     def name = node.name()
     return (name instanceof QName &&
             name.localPart == 'Reference' &&
             node.'@type' == 'BUSINESS_TERM')
 }
-
-//noinspection GroovyAssignabilityCheck
 references.sort  { Node a, Node b ->
-    String atext = a.text()
-    String btext = b.text()
+    String atext = a.text().split(',')[0]
+    String btext = b.text().split(',')[0]
     def prefixCompare = atext.substring(0, 2) <=> btext.substring(0, 2)
     if (prefixCompare == 0) {
         atext.replaceAll("\\D", "").toInteger() <=> btext.replaceAll("\\D", "").toInteger()
