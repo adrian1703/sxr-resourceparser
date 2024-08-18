ddf93bab00bb7beaafe624b49c2a20433ba739be
adrian <adrian.kuhn@arcor.de>
2024-08-18 16:46:48 +0200

    script to print all existing buisness terms in peepol model

--- modelgen/src/script.groovy
+++ modelgen/src/script.groovy
@@ -1,3 +1,17 @@
 import groovy.xml.XmlParser
+import groovy.namespace.QName
 
-XmlParser parser = new XmlParser()
+String path = "./../../resources/peppol-bis-invoice-3/structure/syntax/ubl-invoice.xml"
+XmlParser parser = new XmlParser(namespaceAware: false)
+Node root = parser.parse(path)
+List<Node> references = root.'**'.findAll {Node node ->
+    def name = node.name()
+    return (name instanceof QName &&
+            name.localPart == 'Reference' &&
+            node.'@type' == 'BUSINESS_TERM')
+}
+references.sort { a, b ->
+    a.text().replaceAll("\\D", "").toInteger() <=> b.text().replaceAll("\\D", "").toInteger()
+}
+println(references.size())
+references.each {println it.text()}
