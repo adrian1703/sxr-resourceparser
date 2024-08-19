418bee96fdcc4cb324bcfa88684a56558fe45c76
adrian <adrian.kuhn@arcor.de>
2024-08-19 20:35:38 +0200

    extract reading xml

---
+++ modelgen/src/Utils.groovy
@@ -0,0 +1,24 @@
+import groovy.xml.XmlParser
+import groovy.namespace.QName
+/* only work with patch-2 of my fork */
+class Utils {
+
+    static Node readXml(String fileName) {
+        String path = "./../../resources/peppol-bis-invoice-3/structure/syntax"
+        String rootFile = "$path/$fileName"
+        XmlParser parser = new XmlParser(namespaceAware: true)
+        Node root = parser.parse(rootFile)
+        //resolving includes
+        List<Node> includeNodes = root.'**'.findAll{ Node node ->
+            def name = node.name()
+            return (name instanceof QName &&
+                    name.localPart == 'Include')
+        }
+        includeNodes.each { Node node ->
+            String includePath = node.text()
+            Node nodeToInclude = parser.parse("$path/$includePath")
+            node.replaceNode(nodeToInclude)
+        }
+        return root
+    }
+}
