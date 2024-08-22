dbf55c63e26d33bbaef0cf4f44882985b417764e
adrian <adrian.kuhn@arcor.de>
2024-08-22 23:34:48 +0200

    removed namespaces from parser

--- modelgen/src/Utils.groovy
+++ modelgen/src/Utils.groovy
@@ -1,5 +1,6 @@
 import groovy.xml.XmlParser
 import groovy.namespace.QName
+import groovy.xml.XmlUtil
 import groovy.xml.slurpersupport.GPathResult
 
 /* only work with patch-2 of my fork */
@@ -8,7 +9,7 @@ class Utils {
     static Node readXml(String fileName) {
         String path = "./../../resources/peppol-bis-invoice-3/structure/syntax"
         String rootFile = "$path/$fileName"
-        XmlParser parser = new XmlParser(namespaceAware: true)
+        XmlParser parser = new XmlParser(namespaceAware: false)
         Node root = parser.parse(rootFile)
         //resolving includes
         List<Node> includeNodes = root.'**'.findAll{ Node node ->
@@ -21,10 +22,20 @@ class Utils {
             Node nodeToInclude = parser.parse("$path/$includePath")
             node.replaceNode(nodeToInclude)
         }
+        root = parser.parseText(XmlUtil.serialize(root)
+                .replace(
+                        'xmlns="urn:fdc:difi.no:2017:vefa:structure-1"',
+                        ''))
+        root.'**'.each{
+            if(it instanceof Node
+                    && it.children()[0] instanceof String)
+                it.setValue(it.children()[0].trim())
+        }
         return root
     }
 
-    static List getElementsWithDataTypes(GPathResult node){
+    static List getElementsWithDataTypes(def node){
         return node.'**'.findAll{ it.name() == "Element" && it.DataType.text() != ""}
     }
+
 }
